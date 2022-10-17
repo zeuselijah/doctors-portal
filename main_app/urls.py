3b5201fc34719bc8da6d7751c164f7d9ecfa916a
zeuselijah <simeonhendricks@gmail.com>
2022-10-17 16:55:22 -0400

    wip: modifies urls.py views.py style.css about.html base.html index.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('patients/', views.patients_index, name='patients_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Hello Hospitals</h1>')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def patients_index(request):
+    return render(request, 'patients/index.html', {'patients': patients})
+
+class Patient:
+    def __init__(self, name, age, gender, chief_complaint, current_condition, past_medical_history):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.chief_complaint = chief_complaint
+        self.current_condition = current_condition
+        self.past_medical_history = past_medical_history
+
+patients = [
+    Patient('Test One', 21, 'Male', 'Difficulty in Breathing', 'Labored Breathing', 'Asthma'),
+    Patient('Test Two', 22, 'Female', 'Chest Pain', 'Heart beating fast', 'Heart Failure'),
+    Patient('Test Three', 23, 'Male', 'Injury from fall', 'Head pain and bleeding from the head', 'None'),
+] 
