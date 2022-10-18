5dc1cc83aab6a4af76378613b5d2e4260661bfa8
zeuselijah <simeonhendricks@gmail.com>
2022-10-18 10:12:08 -0400

    wip: adds first model and makes first migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    chief_complaint = models.TextField(max_length=500)
+    current_condition = models.TextField(max_length=500)
+    past_medical_history = models.TextField(max_length=500)
+
+def __str__(self):
+    return self.name
