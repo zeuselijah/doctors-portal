4961a293eacf17b3240a77d89fe8fe15aa420555
zeuselijah <simeonhendricks@gmail.com>
2022-10-18 10:35:29 -0400

    wip: updates admin.py urls.py views.py index.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from . models import Patient
 
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
@@ -9,19 +9,7 @@ def about(request):
     return render(request, 'about.html')
 
 def patients_index(request):
+    patients = Patient.objects.all()
     return render(request, 'patients/index.html', {'patients': patients})
 
-class Patient:
-    def __init__(self, name, age, gender, chief_complaint, current_condition, past_medical_history):
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self.chief_complaint = chief_complaint
-        self.current_condition = current_condition
-        self.past_medical_history = past_medical_history
 
-patients = [
-    Patient('Test One', 21, 'Male', 'Difficulty in Breathing', 'Labored Breathing', 'Asthma'),
-    Patient('Test Two', 22, 'Female', 'Chest Pain', 'Heart beating fast', 'Heart Failure'),
-    Patient('Test Three', 23, 'Male', 'Injury from fall', 'Head pain and bleeding from the head', 'None'),
-] 
